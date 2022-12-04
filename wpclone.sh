d26cfaa2e8e812b5fea292eebc72e8041680e663
Robert Powell <51504171+robpowns@users.noreply.github.com>
2022-12-04 18:17:27 +0000

    Update wpclone.sh

--- wpclone.sh
+++ wpclone.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 #####################################
 # Author: Robbie Powell
 # Version: v1.0.0
@@ -10,8 +8,8 @@
 # Variables ##############
 #$1 Source domain
 #$2 Destination domain
-sdir=awk -F"[:= ]" '/$1/{print $11}' /etc/userdatadomains | column -t | head -n1
-ddir=awk -F"[:= ]" '/$2/{print $11}' /etc/userdatadomains | column -t | head -n1
+sdir=$(awk -F"[:= ]" '/$1/{print $11}' /etc/userdatadomains | column -t | head -n1)
+ddir=$(awk -F"[:= ]" '/$2/{print $11}' /etc/userdatadomains | column -t | head -n1)
 ####################################
 
 
@@ -21,9 +19,9 @@ echo "==================================================="
 
 #check source is wordpress site
 #checks for wp-config file
-if  test -f "$1"; then
-        echo "Wordpress site present at source $1"
-        else
+if  test -f "$1"; then
+        echo "Wordpress site present at source $1"
+        else
 echo "Source does not appear to be a Wordpress install"
 
 fi
@@ -32,35 +30,37 @@ fi
 
 if [ -d "$2" ]
 then
-	if [ "$(ls -A $2)" ]; then
-     echo "Take action $DIR is not Empty"
+	if [ "$(ls -A "$2")" ]; then
+     echo "Take action $ddir is not Empty"
      exit
 	else
-    echo "$DIR is Empty"
+    echo "$ddir is Empty"
 	fi
   else
-	echo "Directory $DIR not found."
+	echo "Directory $ddir not found."
 fi
 
 #copy files from source domain to destination domain
 
-cp $sdir $ddir
+cp "$sdir" "$ddir"
 
 
 #prompt to enter destination db and db_user update destination wp-config
-read db_name
-read db_user
-read db_user_pw
+read -r db_name
+read -r db_user
+read -r db_user_pw
 #dump db (check source config file for details) copy source db to destination
-WPDBNAME=`cat wp-config.php | grep DB_NAME | cut -d \' -f 4`
-WPDBUSER=`cat wp-config.php | grep DB_USER | cut -d \' -f 4`
-WPDBPASS=`cat wp-config.php | grep DB_PASSWORD | cut -d \' -f 4`
+WPDBNAME=$(cat wp-config.php | grep DB_NAME | cut -d \' -f 4)
+WPDBUSER=$(cat wp-config.php | grep DB_USER | cut -d \' -f 4)
+WPDBPASS=$(cat wp-config.php | grep DB_PASSWORD | cut -d \' -f 4)
 
-mysqldump –u $WPDBUSER  –p $WPDBPASS $WPDBNAME > source.sql
-cd $ddir wp config create --dbname=$db_name--dbuser=db_user --dbpass=db_user_pw
+mysqldump –u "$WPDBUSER"  –p "$WPDBPASS" "$WPDBNAME" > source.sql
+cd "$ddir" || exit
+sudo -H -u  bash -c 
+wp config create --dbname="$db_name"--dbuser="$db_user" --dbpass="$db_user_pw"
 #destination
-mysql -u dbname=$db_name  -p db_user_pw $db_name < source.sql
+mysql -u dbname="$db_name"  -p db_user_pw "$db_name" < source.sql
 
 
 #search and replace on destination site from $1 to $2 will need to run command as user
-cd $ddir wp search-replace \"$1\" \"$2\" --all-tables --precise;\
+cd  "$ddir" wp search-replace "$1" "$2" --all-tables --precise || exit;
